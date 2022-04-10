b7dab3c8cd39900902ace88424750e1b967df18a
ArvinthSenthil <arvinthraj1999@gmail.com>
2022-04-10 15:32:02 +0530

    updated

---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'St Joesph Matriculation Higher Secondary School',
+        course: 'Higher Secondary Education',
+        startYear: '2011',
+        endYear: '2015'
+    },
+    {
+        id: 2,
+        institution: 'Nettur Technical Training Foundation',
+        course: 'Diploma In Mechatronics',
+        startYear: '2015',
+        endYear: '2018'
+    },
+    {
+        id: 3,
+        institution: 'Indian Institue Of Industry Interaction Education And Research',
+        course: 'BTech Mechatronics',
+        startYear: '2018',
+        endYear: '2021'
+    },
+]
---
+++ src/data/experienceData.js
@@ -0,0 +1,17 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Dopag India',
+        jobtitle: 'Assembly Trainee',
+        startYear: '2018 July',
+        endYear: '2018 December'
+    },
+    {
+        id: 2,
+        company: 'Delta Electronics',
+        jobtitle: 'Industrial Engineer',
+        startYear: '2019',
+        endYear: 'Present'
+    },
+   
+]
